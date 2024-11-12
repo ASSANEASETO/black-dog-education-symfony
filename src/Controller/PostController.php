5212b8e000e0bc20702e1093477192b32bbc2e9d
Assane GNING <gningaseto@hotmail.fr>
2024-11-12 01:00:07 +0100

    Déclaration de confidatialité

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
 class PostController extends AbstractController
 {
     #[Route('/blog', name: 'app_post', methods:['GET', 'POST'])]
